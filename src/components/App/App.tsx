9551ff05040db9a48045772b0b04fe2c1dce0fc4
IVKrylova <ivk.lis@yandex.ru>
2022-08-24 20:23:43 +0300

    style: add font roboto

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
 
     </div>
   );
