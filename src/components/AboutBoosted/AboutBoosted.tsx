653dbb2000eeca72766ab0774572802e30debc95
IVKrylova <ivk.lis@yandex.ru>
2022-09-14 13:26:18 +0300

    feat: add html and css code AboutBoosted

--- src/components/AboutBoosted/AboutBoosted.tsx
+++ src/components/AboutBoosted/AboutBoosted.tsx
@@ -7,23 +7,74 @@ const AboutBoosted: React.FC = () => {
     <section className={styles.aboutBoosted}>
       <img src={logoBoosted} alt="logo Boosted USA" className={styles.aboutBoosted__logo} />
       <h3 className={`${styles.aboutBoosted__title} ${styles.aboutBoosted__title_place_links}`}>
-
+        Explore
       </h3>
       <ul className={styles.aboutBoosted__links}>
-        <li className={styles.aboutBoosted__link}></li>
-        <li className={styles.aboutBoosted__link}></li>
-        <li className={styles.aboutBoosted__link}></li>
-        <li className={styles.aboutBoosted__link}></li>
-        <li className={styles.aboutBoosted__link}></li>
-        <li className={styles.aboutBoosted__link}></li>
-        <li className={styles.aboutBoosted__link}></li>
-        <li className={styles.aboutBoosted__link}></li>
-        <li className={styles.aboutBoosted__link}></li>
+        <li>
+          <a className={styles.aboutBoosted__link}>
+            Electric Skateboards
+          </a>
+        </li>
+        <li>
+          <a className={styles.aboutBoosted__link}>
+            Electric Scooters
+          </a>
+        </li>
+        <li>
+          <a className={styles.aboutBoosted__link}>
+            Accessories
+          </a>
+        </li>
+        <li>
+          <a className={styles.aboutBoosted__link}>
+            FAQs
+          </a>
+        </li>
+        <li>
+          <a className={styles.aboutBoosted__link}>
+            Warranty
+          </a>
+        </li>
+        <li>
+          <a className={styles.aboutBoosted__link}>
+            Quick Start Guide
+          </a>
+        </li>
+        <li>
+          <a className={styles.aboutBoosted__link}>
+            Contact
+          </a>
+        </li>
+        <li>
+          <a className={styles.aboutBoosted__link}>
+            Gift Card
+          </a>
+        </li>
+        <li>
+          <a className={styles.aboutBoosted__link}>
+            Accessibility Statement
+          </a>
+        </li>
       </ul>
       <h3 className={`${styles.aboutBoosted__title} ${styles.aboutBoosted__title_place_text}`}>
-
+        About Boosted USA
       </h3>
-      <p className={styles.aboutBoosted__text}></p>
+      <p className={styles.aboutBoosted__text}>
+        Boosted empowers people everywhere to commute across their cities, campuses,
+        and communities in ways that were never before possible. Boosted is solving
+        one of the biggest problems people face each day: transportation.
+      </p>
+      <address className={styles.aboutBoosted__address}>
+        <span className={`${styles.aboutBoosted__line} ${styles.aboutBoosted__line_newLine}`}>
+          CaliRides LLC - DBA Boosted USA
+        </span>
+        <span className={`${styles.aboutBoosted__line} ${styles.aboutBoosted__line_newLine}`}>
+          1281 Andersen Drive Ste. K
+        </span>
+        <span className={`${styles.aboutBoosted__line} ${styles.aboutBoosted__line_newLine}`}>
+          San Rafael, CA 94901
+        </span>
+      </address>
   </section>
   );
 }
