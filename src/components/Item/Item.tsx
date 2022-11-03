15ac95728f3cfba12c5c9dffb2e54f7622037f06
IVKrylova <ivk.lis@yandex.ru>
2022-11-03 14:42:51 +0300

    feat: style: add style by the button in Item

--- src/components/Item/Item.tsx
+++ src/components/Item/Item.tsx
@@ -1,4 +1,5 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
+import { useTypeSelector } from '../../hooks/useTypeSelector';
 import styles from './Item.module.scss';
 
 interface ItemProps {
@@ -11,12 +12,24 @@ interface ItemProps {
 }
 
 const Item: FC<ItemProps> = ({ item }) => {
+  const [isButtonDisabled, setIsButtonDisabled] = useState<boolean>(false);
+  const { cart } = useTypeSelector(state => state.cart);
+
+  useEffect(() => {
+    if (cart.some(el => el.id === item.id)) setIsButtonDisabled(true);
+  }, []);
+
   return (
     <main className={styles.item}>
       <img src={item.img} alt={item.name} className={styles.item__img} />
       <h1 className={styles.item__name}>{item.name}</h1>
       <p className={styles.item__price}>{`${item.price} $`}</p>
-      <button type='button' className={styles.item__button}>To Cart</button>
+      <button
+        type='button'
+        className={`${styles.item__button} ${isButtonDisabled ? styles.item__button_disabled : ''}`}
+      >
+        {isButtonDisabled ? 'In Cart' : 'To Cart'}
+      </button>
     </main>
   );
 }
